034aa3e0b2575db1d17308c765a38101d5701c26
andrew <andrew@localhost>
2019-09-02 20:57:15 +0200

    kill TL_contacts_search

--- TMessagesProj/src/main/java/org/telegram/ui/Adapters/SearchAdapterHelper.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Adapters/SearchAdapterHelper.java
@@ -163,110 +163,110 @@ public class SearchAdapterHelper {
             channelLastReqId = 0;
             delegate.onDataSetChanged();
         }
-        if (allowUsername) {
-            if (query.length() > 0) {
-                TLRPC.TL_contacts_search req = new TLRPC.TL_contacts_search();
-                req.q = query;
-                req.limit = 50;
-                final int currentReqId = ++lastReqId;
-                reqId = ConnectionsManager.getInstance(currentAccount).sendRequest(req, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
-                    if (currentReqId == lastReqId) {
-                        reqId = 0;
-                        if (error == null) {
-                            TLRPC.TL_contacts_found res = (TLRPC.TL_contacts_found) response;
-                            globalSearch.clear();
-                            globalSearchMap.clear();
-                            localServerSearch.clear();
-                            MessagesController.getInstance(currentAccount).putChats(res.chats, false);
-                            MessagesController.getInstance(currentAccount).putUsers(res.users, false);
-                            MessagesStorage.getInstance(currentAccount).putUsersAndChats(res.users, res.chats, true, true);
-                            SparseArray<TLRPC.Chat> chatsMap = new SparseArray<>();
-                            SparseArray<TLRPC.User> usersMap = new SparseArray<>();
-                            for (int a = 0; a < res.chats.size(); a++) {
-                                TLRPC.Chat chat = res.chats.get(a);
-                                chatsMap.put(chat.id, chat);
-                            }
-                            for (int a = 0; a < res.users.size(); a++) {
-                                TLRPC.User user = res.users.get(a);
-                                usersMap.put(user.id, user);
-                            }
-                            for (int b = 0; b < 2; b++) {
-                                ArrayList<TLRPC.Peer> arrayList;
-                                if (b == 0) {
-                                    if (!allResultsAreGlobal) {
-                                        continue;
-                                    }
-                                    arrayList = res.my_results;
-                                } else {
-                                    arrayList = res.results;
-                                }
-                                for (int a = 0; a < arrayList.size(); a++) {
-                                    TLRPC.Peer peer = arrayList.get(a);
-                                    TLRPC.User user = null;
-                                    TLRPC.Chat chat = null;
-                                    if (peer.user_id != 0) {
-                                        user = usersMap.get(peer.user_id);
-                                    } else if (peer.chat_id != 0) {
-                                        chat = chatsMap.get(peer.chat_id);
-                                    } else if (peer.channel_id != 0) {
-                                        chat = chatsMap.get(peer.channel_id);
-                                    }
-                                    if (chat != null) {
-                                        if (!allowChats) {
-                                            continue;
-                                        }
-                                        globalSearch.add(chat);
-                                        globalSearchMap.put(-chat.id, chat);
-                                    } else if (user != null) {
-                                        if (!allowBots && user.bot || !allowSelf && user.self) {
-                                            continue;
-                                        }
-                                        globalSearch.add(user);
-                                        globalSearchMap.put(user.id, user);
-                                    }
-                                }
-                            }
-                            if (!allResultsAreGlobal) {
-                                for (int a = 0; a < res.my_results.size(); a++) {
-                                    TLRPC.Peer peer = res.my_results.get(a);
-                                    TLRPC.User user = null;
-                                    TLRPC.Chat chat = null;
-                                    if (peer.user_id != 0) {
-                                        user = usersMap.get(peer.user_id);
-                                    } else if (peer.chat_id != 0) {
-                                        chat = chatsMap.get(peer.chat_id);
-                                    } else if (peer.channel_id != 0) {
-                                        chat = chatsMap.get(peer.channel_id);
-                                    }
-                                    if (chat != null) {
-                                        if (!allowChats) {
-                                            continue;
-                                        }
-                                        localServerSearch.add(chat);
-                                        globalSearchMap.put(-chat.id, chat);
-                                    } else if (user != null) {
-                                        localServerSearch.add(user);
-                                        globalSearchMap.put(user.id, user);
-                                    }
-                                }
-                            }
-                            lastFoundUsername = query.toLowerCase();
-                            if (localSearchResults != null) {
-                                mergeResults(localSearchResults);
-                            }
-                            mergeExcludeResults();
-                            delegate.onDataSetChanged();
-                        }
-                    }
-                }), ConnectionsManager.RequestFlagFailOnServerErrors);
-            } else {
-                globalSearch.clear();
-                globalSearchMap.clear();
-                localServerSearch.clear();
-                lastReqId = 0;
-                delegate.onDataSetChanged();
-            }
-        }
+//        if (allowUsername) {
+//            if (query.length() > 0) {
+//                TLRPC.TL_contacts_search req = new TLRPC.TL_contacts_search();
+//                req.q = query;
+//                req.limit = 50;
+//                final int currentReqId = ++lastReqId;
+//                reqId = ConnectionsManager.getInstance(currentAccount).sendRequest(req, (response, error) -> AndroidUtilities.runOnUIThread(() -> {
+//                    if (currentReqId == lastReqId) {
+//                        reqId = 0;
+//                        if (error == null) {
+//                            TLRPC.TL_contacts_found res = (TLRPC.TL_contacts_found) response;
+//                            globalSearch.clear();
+//                            globalSearchMap.clear();
+//                            localServerSearch.clear();
+//                            MessagesController.getInstance(currentAccount).putChats(res.chats, false);
+//                            MessagesController.getInstance(currentAccount).putUsers(res.users, false);
+//                            MessagesStorage.getInstance(currentAccount).putUsersAndChats(res.users, res.chats, true, true);
+//                            SparseArray<TLRPC.Chat> chatsMap = new SparseArray<>();
+//                            SparseArray<TLRPC.User> usersMap = new SparseArray<>();
+//                            for (int a = 0; a < res.chats.size(); a++) {
+//                                TLRPC.Chat chat = res.chats.get(a);
+//                                chatsMap.put(chat.id, chat);
+//                            }
+//                            for (int a = 0; a < res.users.size(); a++) {
+//                                TLRPC.User user = res.users.get(a);
+//                                usersMap.put(user.id, user);
+//                            }
+//                            for (int b = 0; b < 2; b++) {
+//                                ArrayList<TLRPC.Peer> arrayList;
+//                                if (b == 0) {
+//                                    if (!allResultsAreGlobal) {
+//                                        continue;
+//                                    }
+//                                    arrayList = res.my_results;
+//                                } else {
+//                                    arrayList = res.results;
+//                                }
+//                                for (int a = 0; a < arrayList.size(); a++) {
+//                                    TLRPC.Peer peer = arrayList.get(a);
+//                                    TLRPC.User user = null;
+//                                    TLRPC.Chat chat = null;
+//                                    if (peer.user_id != 0) {
+//                                        user = usersMap.get(peer.user_id);
+//                                    } else if (peer.chat_id != 0) {
+//                                        chat = chatsMap.get(peer.chat_id);
+//                                    } else if (peer.channel_id != 0) {
+//                                        chat = chatsMap.get(peer.channel_id);
+//                                    }
+//                                    if (chat != null) {
+//                                        if (!allowChats) {
+//                                            continue;
+//                                        }
+//                                        globalSearch.add(chat);
+//                                        globalSearchMap.put(-chat.id, chat);
+//                                    } else if (user != null) {
+//                                        if (!allowBots && user.bot || !allowSelf && user.self) {
+//                                            continue;
+//                                        }
+//                                        globalSearch.add(user);
+//                                        globalSearchMap.put(user.id, user);
+//                                    }
+//                                }
+//                            }
+//                            if (!allResultsAreGlobal) {
+//                                for (int a = 0; a < res.my_results.size(); a++) {
+//                                    TLRPC.Peer peer = res.my_results.get(a);
+//                                    TLRPC.User user = null;
+//                                    TLRPC.Chat chat = null;
+//                                    if (peer.user_id != 0) {
+//                                        user = usersMap.get(peer.user_id);
+//                                    } else if (peer.chat_id != 0) {
+//                                        chat = chatsMap.get(peer.chat_id);
+//                                    } else if (peer.channel_id != 0) {
+//                                        chat = chatsMap.get(peer.channel_id);
+//                                    }
+//                                    if (chat != null) {
+//                                        if (!allowChats) {
+//                                            continue;
+//                                        }
+//                                        localServerSearch.add(chat);
+//                                        globalSearchMap.put(-chat.id, chat);
+//                                    } else if (user != null) {
+//                                        localServerSearch.add(user);
+//                                        globalSearchMap.put(user.id, user);
+//                                    }
+//                                }
+//                            }
+//                            lastFoundUsername = query.toLowerCase();
+//                            if (localSearchResults != null) {
+//                                mergeResults(localSearchResults);
+//                            }
+//                            mergeExcludeResults();
+//                            delegate.onDataSetChanged();
+//                        }
+//                    }
+//                }), ConnectionsManager.RequestFlagFailOnServerErrors);
+//            } else {
+//                globalSearch.clear();
+//                globalSearchMap.clear();
+//                localServerSearch.clear();
+//                lastReqId = 0;
+//                delegate.onDataSetChanged();
+//            }
+//        }
         if (phoneNumbers && query.startsWith("+") && query.length() > 3) {
             phonesSearch.clear();
             phoneSearchMap.clear();
