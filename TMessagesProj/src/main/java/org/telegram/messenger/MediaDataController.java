45b741e1f179ab49f8a5d49d266b56ec0bbbf337
andrew <andrew@localhost>
2019-09-02 20:18:23 +0200

    kill TL_messages_getFavedStickers

--- TMessagesProj/src/main/java/org/telegram/messenger/MediaDataController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MediaDataController.java
@@ -666,7 +666,7 @@ public class MediaDataController extends BaseController {
                 cache = false;
             }
         }
-        if (cache) {
+//        if (cache) {
             getMessagesStorage().getStorageQueue().postRunnable(() -> {
                 try {
                     final int cacheType;
@@ -711,68 +711,68 @@ public class MediaDataController extends BaseController {
                     FileLog.e(e);
                 }
             });
-        } else {
-            SharedPreferences preferences = MessagesController.getEmojiSettings(currentAccount);
-            if (!force) {
-                long lastLoadTime;
-                if (gif) {
-                    lastLoadTime = preferences.getLong("lastGifLoadTime", 0);
-                } else if (type == TYPE_IMAGE) {
-                    lastLoadTime = preferences.getLong("lastStickersLoadTime", 0);
-                } else if (type == TYPE_MASK) {
-                    lastLoadTime = preferences.getLong("lastStickersLoadTimeMask", 0);
-                } else {
-                    lastLoadTime = preferences.getLong("lastStickersLoadTimeFavs", 0);
-                }
-                if (Math.abs(System.currentTimeMillis() - lastLoadTime) < 60 * 60 * 1000) {
-                    if (gif) {
-                        loadingRecentGifs = false;
-                    } else {
-                        loadingRecentStickers[type] = false;
-                    }
-                    return;
-                }
-            }
-            if (gif) {
-                TLRPC.TL_messages_getSavedGifs req = new TLRPC.TL_messages_getSavedGifs();
-                req.hash = calcDocumentsHash(recentGifs);
-                getConnectionsManager().sendRequest(req, (response, error) -> {
-                    ArrayList<TLRPC.Document> arrayList = null;
-                    if (response instanceof TLRPC.TL_messages_savedGifs) {
-                        TLRPC.TL_messages_savedGifs res = (TLRPC.TL_messages_savedGifs) response;
-                        arrayList = res.gifs;
-                    }
-                    processLoadedRecentDocuments(type, arrayList, gif, 0, true);
-                });
-            } else {
-                TLObject request;
-                if (type == TYPE_FAVE) {
-                    TLRPC.TL_messages_getFavedStickers req = new TLRPC.TL_messages_getFavedStickers();
-                    req.hash = calcDocumentsHash(recentStickers[type]);
-                    request = req;
-                } else {
-                    TLRPC.TL_messages_getRecentStickers req = new TLRPC.TL_messages_getRecentStickers();
-                    req.hash = calcDocumentsHash(recentStickers[type]);
-                    req.attached = type == TYPE_MASK;
-                    request = req;
-                }
-                getConnectionsManager().sendRequest(request, (response, error) -> {
-                    ArrayList<TLRPC.Document> arrayList = null;
-                    if (type == TYPE_FAVE) {
-                        if (response instanceof TLRPC.TL_messages_favedStickers) {
-                            TLRPC.TL_messages_favedStickers res = (TLRPC.TL_messages_favedStickers) response;
-                            arrayList = res.stickers;
-                        }
-                    } else {
-                        if (response instanceof TLRPC.TL_messages_recentStickers) {
-                            TLRPC.TL_messages_recentStickers res = (TLRPC.TL_messages_recentStickers) response;
-                            arrayList = res.stickers;
-                        }
-                    }
-                    processLoadedRecentDocuments(type, arrayList, gif, 0, true);
-                });
-            }
-        }
+//        } else {
+//            SharedPreferences preferences = MessagesController.getEmojiSettings(currentAccount);
+//            if (!force) {
+//                long lastLoadTime;
+//                if (gif) {
+//                    lastLoadTime = preferences.getLong("lastGifLoadTime", 0);
+//                } else if (type == TYPE_IMAGE) {
+//                    lastLoadTime = preferences.getLong("lastStickersLoadTime", 0);
+//                } else if (type == TYPE_MASK) {
+//                    lastLoadTime = preferences.getLong("lastStickersLoadTimeMask", 0);
+//                } else {
+//                    lastLoadTime = preferences.getLong("lastStickersLoadTimeFavs", 0);
+//                }
+//                if (Math.abs(System.currentTimeMillis() - lastLoadTime) < 60 * 60 * 1000) {
+//                    if (gif) {
+//                        loadingRecentGifs = false;
+//                    } else {
+//                        loadingRecentStickers[type] = false;
+//                    }
+//                    return;
+//                }
+//            }
+//            if (gif) {
+//                TLRPC.TL_messages_getSavedGifs req = new TLRPC.TL_messages_getSavedGifs();
+//                req.hash = calcDocumentsHash(recentGifs);
+//                getConnectionsManager().sendRequest(req, (response, error) -> {
+//                    ArrayList<TLRPC.Document> arrayList = null;
+//                    if (response instanceof TLRPC.TL_messages_savedGifs) {
+//                        TLRPC.TL_messages_savedGifs res = (TLRPC.TL_messages_savedGifs) response;
+//                        arrayList = res.gifs;
+//                    }
+//                    processLoadedRecentDocuments(type, arrayList, gif, 0, true);
+//                });
+//            } else {
+//                TLObject request;
+//                if (type == TYPE_FAVE) {
+//                    TLRPC.TL_messages_getFavedStickers req = new TLRPC.TL_messages_getFavedStickers();
+//                    req.hash = calcDocumentsHash(recentStickers[type]);
+//                    request = req;
+//                } else {
+//                    TLRPC.TL_messages_getRecentStickers req = new TLRPC.TL_messages_getRecentStickers();
+//                    req.hash = calcDocumentsHash(recentStickers[type]);
+//                    req.attached = type == TYPE_MASK;
+//                    request = req;
+//                }
+//                getConnectionsManager().sendRequest(request, (response, error) -> {
+//                    ArrayList<TLRPC.Document> arrayList = null;
+//                    if (type == TYPE_FAVE) {
+//                        if (response instanceof TLRPC.TL_messages_favedStickers) {
+//                            TLRPC.TL_messages_favedStickers res = (TLRPC.TL_messages_favedStickers) response;
+//                            arrayList = res.stickers;
+//                        }
+//                    } else {
+//                        if (response instanceof TLRPC.TL_messages_recentStickers) {
+//                            TLRPC.TL_messages_recentStickers res = (TLRPC.TL_messages_recentStickers) response;
+//                            arrayList = res.stickers;
+//                        }
+//                    }
+//                    processLoadedRecentDocuments(type, arrayList, gif, 0, true);
+//                });
+//            }
+//        }
     }
 
     protected void processLoadedRecentDocuments(final int type, final ArrayList<TLRPC.Document> documents, final boolean gif, final int date, boolean replace) {
